26ef180608fdc726c6dee6519a0c8097b05dfeb2
Heming666 <hm80@vip.qq.com>
2021-04-30 15:58:11 +0800

    数据库上下文

---
+++ Util.Log/ILoggerFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Util.Log
+{
+    public interface ILoggerFactory
+    {
+        void Info(string message);
+        void Debug(string message);
+        void Warn(string message);
+        void Error(string message);
+        void Error(Exception ex, string message);
+        void Info<T>(T logEntity);
+        void Debug<T>(T logEntity);
+        void Warn<T>(T logEntity);
+        void Error<T>(T logEntity);
+    }
+}
