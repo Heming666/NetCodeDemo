0ad0f3c9e3dae9a6f40a82c8087578bc17c6dd5d
何明 <@DESKTOP-9J860KJ>
2021-05-20 17:52:52 +0800

    表单页面修改

--- Demo.Web/Controllers/LoginController.cs
+++ Demo.Web/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using Business.IService.Base;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Repository.Entity.Models.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,23 +13,33 @@ namespace Demo.Web.Controllers
     {
         public readonly ILoggerFactory _factory;
         public readonly IUserService _userService;
-        public LoginController(ILoggerFactory factory,IUserService userService)
+        public readonly IDepartmentService _deptService;
+        public LoginController(ILoggerFactory factory,IUserService userService,IDepartmentService deptService)
         {
             _factory = factory;
             _userService = userService;
+            _deptService = deptService;
         }
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Login()
         {
             return View();
         }
-        public IActionResult Regist()
-        {
-            return View();
-        }
+  
 
+        /// <summary>
+        /// 注册页面
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Register()
         {
-            return View();
+            UserEntity entity = new UserEntity() { CreateDate = DateTime.Now };
+            List<DepartmentEntity> depts = _deptService.GetList(Util.Extension.ExpressionExtension.True<DepartmentEntity>());
+            ViewBag.Depts = depts;
+            return View(entity);
         }
 
     }
--- Repository.Entity/Models/Base/UserEntity.cs
+++ Repository.Entity/Models/Base/UserEntity.cs
@@ -24,42 +24,43 @@ namespace Repository.Entity.Models.Base
         /// <summary>
         /// 账户
         /// </summary>
-        [Required(ErrorMessage ="账号不能为空"),MinLength(6),MaxLength(24), Comment("账户")]
+        [Required(ErrorMessage ="账号不能为空"),MinLength(6),MaxLength(24), Comment("账户"), Display(Name = "账号")]
         //[ConcurrencyCheck]并发标记
         public string  Account { get; set; }
 
         /// <summary>
         /// 密码
         /// </summary>
-        [Required(ErrorMessage = "密码不能为空"), MinLength(6), MaxLength(24), Comment("密码")]
+        [Required(ErrorMessage = "密码不能为空"), MinLength(6), MaxLength(24), Comment("密码"),DataType(DataType.Password), Display(Name = "密码")]
         public string  PassWord { get; set; }
 
         /// <summary>
         /// 用户昵称
         /// </summary>
-        [Required(ErrorMessage = "用户昵称不能为空"), MinLength(1), MaxLength(24), Comment("用户昵称")]
+        [Required(ErrorMessage = "用户昵称不能为空"), MinLength(1), MaxLength(24), Comment("用户昵称"), Display(Name = "昵称")]
         public string  UserName { get; set; }
 
         /// <summary>
         /// 性别
         /// </summary>2
-        [Column(TypeName = "int"), Comment("性别")]
+        [Column(TypeName = "int"), Comment("性别"),Display(Name ="性别")]
         public Gender? Gender { get; set; }
         /// <summary>
         /// 用户昵称
         /// </summary>
-        [Comment("照片"),MaxLength(500)]
+        [Comment("照片"),MaxLength(500),DataType(DataType.ImageUrl), Display(Name = "照片")]
         public string  Photo { get; set; }
         /// <summary>
         /// 手机号
         /// </summary>
-        [RegularExpression("^1[34578]\\d{9}$",ErrorMessage ="请输入正确的手机号"),MaxLength(11),Comment("手机号")]
+        [RegularExpression("^1[34578]\\d{9}$",ErrorMessage ="请输入正确的手机号"),MaxLength(11),Comment("手机号"), DataType(DataType.PhoneNumber), Display(Name = "手机号")]
         public string  Phone { get; set; }
+
         public DateTime CreateDate { get; set; }
 
         public DateTime  ModifyDate { get; set; }
 
-        [ForeignKey("DeptId")]
+        [ForeignKey("DeptId"), Display(Name = "照片")]
         public int DeptId { get; set; }
         public DepartmentEntity DeptInfo { get; set; }
 
